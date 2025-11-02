db2e29a3c4465b9790617540faf1845a258555f9
Adrian Gavela <adriangavela09@gmail.com>
2025-11-02 17:59:17 +0100

    creacion modelos

---
+++ server/modelo/usuarios.js
@@ -0,0 +1,36 @@
+class usuario{
+    #id;
+    #nombre;
+    #correo;
+    #pass;
+    #dni;
+    #fecha;
+
+    constructor(nombre, correo, pass, fecha, dni){
+        this.#nombre = nombre;
+        this.#correo = correo;
+        this.#pass = pass;
+        this.#fecha = fecha;
+    }
+
+    getNombre(){
+        return this.#nombre;
+    }
+
+    getCorreo(){
+        return this.#correo;
+    }
+
+    getPass(){
+        return this.#pass;
+    }
+
+    getFecha(){
+        return this.#fecha;
+    }
+
+    getDni(){
+        return this.#dni;
+    }
+
+}
